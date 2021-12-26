3e7b77ed059169767cd851b5cbce6b7235fe712e
Sengkham <noultm@gmail.com>
2021-12-26 23:36:12 +0700

    callcenter_v2

---
+++ Callcenter_v2/src/content/overview/Login/index.tsx
@@ -0,0 +1,158 @@
+import {
+    Box,
+    Button,
+    Container,
+    Grid,
+    Typography,
+    TextField
+  } from '@mui/material';
+
+
+  
+  import { Link as RouterLink } from 'react-router-dom';
+   import { styled } from '@mui/material/styles';
+import { Globalstyle } from 'src/fonts/GlobalStyle';
+
+ 
+ 
+  
+  const TypographyH1 = styled(Typography)(
+    ({ theme }) => `
+      font-size: ${theme.typography.pxToRem(30)}; 
+   
+         
+  `
+  );
+  
+  const TypographyH2 = styled(Typography)(
+    ({ theme }) => `
+      font-size: ${theme.typography.pxToRem(17)};
+  `
+  );
+  
+  const LabelWrapper = styled(Box)(
+    ({ theme }) => `
+      background-color: ${theme.colors.success.main};
+      color: ${theme.palette.success.contrastText};
+      font-weight: bold;
+      border-radius: 30px;
+      text-transform: uppercase;
+      display: inline-block;
+      font-size: ${theme.typography.pxToRem(11)};
+      padding: ${theme.spacing(.5)} ${theme.spacing(1.5)};
+      margin-bottom: ${theme.spacing(2)};
+  `
+  );
+  
+  const MuiAvatar = styled(Box)(
+    ({ theme }) => `
+      width: ${theme.spacing(8)};
+      height: ${theme.spacing(8)};
+      border-radius: ${theme.general.borderRadius};
+      background-color: #e5f7ff;
+      flex-shrink: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin: 0 auto ${theme.spacing(2)};
+  
+      img {
+        width: 60%;
+        height: 60%;
+        display: block;
+      }
+  `
+  );
+  
+  const TsAvatar = styled(Box)(
+    ({ theme }) => `
+      width: ${theme.spacing(8)};
+      height: ${theme.spacing(8)};
+      border-radius: ${theme.general.borderRadius};
+      background-color: #dfebf6;
+      flex-shrink: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin: 0 auto ${theme.spacing(2)};
+  
+      img {
+        width: 60%;
+        height: 60%;
+        display: block;
+      }
+  `
+  );
+  
+  function Login() {
+  
+    return (
+    
+      <Container maxWidth="lg" sx={{ textAlign: 'center' }}>
+
+        <Grid spacing={{ xs: 6, md: 10 }} justifyContent="center" alignItems="center" container>
+          <Grid item md={10} lg={8} mx="auto">
+            <LabelWrapper color="success">Version 1.0</LabelWrapper>
+            <TypographyH1 sx={{ mb: 2 }} variant="h1" >
+             Waiwa ( ໄວວາ ) ບໍລິການຂົນສົ່ງ
+            </TypographyH1>
+            <Globalstyle />
+            <TypographyH2
+              sx={{ lineHeight: 1.5, pb: 4 }}
+              variant="h4"
+              color="text.secondary"
+              fontWeight="normal"     
+             
+            >
+             ລະບົບຈັດການໜ້າວຽກການຂົນສົ່ງຂອງ Rider
+            </TypographyH2>
+
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+                >
+                <div>
+                    <TextField         
+                    color='secondary'           
+                    id="usr"
+                    label="ຊື່ຜູ້ໃຊ້"
+                    placeholder='ຊື່ຜູ້ໃຊ້້'
+                    focused 
+                    />
+                    <br/>
+                    <TextField    
+                    color='secondary'                   
+                    id="pwd"
+                    label="ລະຫັດ"
+                    placeholder="ລະຫັດ"  
+                    focused                  
+                    />
+                </div>
+               
+                
+                </Box>
+
+            <br/>
+
+            <Button
+              component={RouterLink}
+              to="/dashboards/crypto"
+              size="large"
+              variant="contained"
+            >
+              ເຂົ້າລະບົບ
+            </Button>
+            
+          
+          </Grid>
+        </Grid>
+      </Container>
+    );
+  }
+  
+  export default Login;
+  
